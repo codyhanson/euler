9c6655cb8263c4494b38131c26f23c1050479379
Cody Hanson <chanson@uwalumni.com>
2017-06-10 21:21:38 -0600

    p1 and p2 done. started p3

---
+++ 1_25/p3.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+Largest prime factor
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+import "fmt"
+
+func main() {
+	target := 600851475143
+	//todo: the math
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+[Project Euler](https://projecteuler.net/) Problems.
+
+![project euler user codyhanson](https://projecteuler.net/profile/codyhanson.png)
